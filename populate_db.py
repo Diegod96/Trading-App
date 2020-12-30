2bbb4ca756658012756e84803280def993a25c82
Diego Delgado <ddphillyfan@gmail.com>
2020-12-30 09:15:58 -0500

    Updated Query

--- populate_db.py
+++ populate_db.py
@@ -1,19 +1,30 @@
 import alpaca_trade_api as tradeapi
 import sqlite3
+import config
 
-connection = sqlite3.connect('app.db')
+connection = sqlite3.connect('C:\\Users\\diego\\Code\\Side Projects\\TradingApp\\app.db')
+connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
-api = tradeapi.REST('PKE0GX8F12QG3S777QIW', 'X3wymZMeLzUgEkfThIWDjeb7nVofJLN9nXJQSHge', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+
+rows = cursor.fetchall()
+
+symbols = [row['symbol'] for row in rows]
+
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY,
+                    base_url='https://paper-api.alpaca.markets')
 assets = api.list_assets()
 
 for asset in assets:
     try:
-        if asset.status == 'active' and asset.tradable:
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
             cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
     except Exception as e:
         print(asset.symbol)
         print(e)
-
 
 connection.commit()
