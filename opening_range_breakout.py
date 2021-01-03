d6febc0ab8e498f7268b7ef606ae2a0807a5f8a1
Diego Delgado <ddphillyfan@gmail.com>
2021-01-03 10:09:25 -0500

    Able to send email message of order data

--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -2,6 +2,7 @@ import sqlite3
 import config
 import alpaca_trade_api as tradeapi
 from datetime import date
+import smtplib, ssl
 
 connection = sqlite3.connect(config.DB_File)
 connection.row_factory = sqlite3.Row
@@ -32,15 +33,17 @@ end_minute_bar = f"{current_date} 09:45:00-04:00"
 
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
 
-orders = api.list_orders(status='all', limit=500, after=f"{current_date}T13:30:00Z")
+orders = api.list_orders()
+# orders = api.list_orders(status='all', limit=500, after=f"{current_date}T13:30:00Z")
 existing_orders_symbols = [order.symbol for order in orders]
-print(existing_orders_symbols)
+# print(existing_orders_symbols)
 
 
 
 
-
+messages = []
 for symbol in symbols:
+    
     minute_bars = api.polygon.historic_agg_v2(
         symbol, 1, 'minute', _from=current_date, to=current_date).df
 
@@ -60,28 +63,37 @@ for symbol in symbols:
         after_opening_range_bars['close'] > opening_range_high]
     if not after_opening_range_breakout.empty:
         if symbol not in existing_orders_symbols:
-            print(after_opening_range_breakout)
+            # print(after_opening_range_breakout)
             limit_price = after_opening_range_breakout.iloc[0]['close']
-            print(limit_price)
+            # print(limit_price)
 
-            print(
-                f"Placing order for {symbol}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
+            messages.append(f"Placing order for {symbol}, closed above {opening_range_high} \n\n {after_opening_range_breakout.iloc[0]}\n\n")
+            print(f"Placing order for {symbol}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
 
-            api.submit_order(
-                symbol=symbol,
-                side='buy',
-                type='limit',
-                qty='100',
-                time_in_force='day',
-                order_class='bracket',
-                limit_price=limit_price,
-                take_profit=dict(
-                    limit_price=limit_price+opening_range,
-                ),
-                stop_loss=dict(
-                    stop_price=limit_price-opening_range,
-                )
-            )
-        else:
-           print(f"Already an order for {symbol}, skipping")
+        #     api.submit_order(
+        #         symbol=symbol,
+        #         side='buy',
+        #         type='limit',
+        #         qty='100',
+        #         time_in_force='day',
+        #         order_class='bracket',
+        #         limit_price=limit_price,
+        #         take_profit=dict(
+        #             limit_price=limit_price+opening_range,
+        #         ),
+        #         stop_loss=dict(
+        #             stop_price=limit_price-opening_range,
+        #         )
+        #     )
+        # else:
+        #    print(f"Already an order for {symbol}, skipping")
+        
+print(messages)
 
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(config.EMAIL_HOST,config.EMAIL_PORT, context=context) as server:
+    server.login(config.EMAIL_SENDER_ADDRESS, config.EMAIL_PASSWORD)
+    email_message = "\n\n".join(messages)
+    server.sendmail(config.EMAIL_SENDER_ADDRESS, config.EMAIL_RECEIVER_ADDRESS, email_message)
+    print("Email sent succesfully")
